cb68ba9c8aa6e87e9c9c46839aba6f66f696cfde
mdinch <mdinc@amaxinsurance.com>
2020-02-11 09:36:29 -0600

    test 2 added

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		int num = 5;
+		
+		if(num >= 5) {
+			System.out.println("it's bigger than five");
+		}else {
+			System.out.println("It's smaller");
+		}
+
+	}
+
+}
